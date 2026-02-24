ea18fec6febe528f64c6bc960e1223abd20531e2
AyushAIWIZ <ayush.f.chauhan@accenture.com>
2026-02-24 06:11:47 +0000

    second

---
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -0,0 +1,27 @@
+from djongo import models
+
+class Team(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Activity(models.Model):
+    user = models.CharField(max_length=100)
+    activity_type = models.CharField(max_length=100)
+    duration = models.IntegerField()
+    team = models.CharField(max_length=100)
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Leaderboard(models.Model):
+    team = models.CharField(max_length=100)
+    points = models.IntegerField()
+    class Meta:
+        app_label = 'octofit_tracker'
+
+class Workout(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    difficulty = models.CharField(max_length=50)
+    class Meta:
+        app_label = 'octofit_tracker'
